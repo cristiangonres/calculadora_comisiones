5d8361df40cf170ab67771443f89a7ae5e15102b
cristian <gonzalez.resina@gmail.com>
2022-11-09 15:59:07 +0100

    correccion2

--- calculadora_comisiones.py
+++ calculadora_comisiones.py
@@ -3,7 +3,7 @@ sales = float(input("¿Ventas realizadas? "))
 
 def calcula_comision(name, sales):
     comision = round(sales*0.13, 2)
-    print(f"El vendedor {name} ha vendidod {sales} y recibe una comisión del 13% que supone un total de: {comision}")
+    print(f"El vendedor {name} ha vendidod {sales}€ y recibe una comisión del 13% que supone un total de: {comision}€")
     
 
 calcula_comision(name, sales)
